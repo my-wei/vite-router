26ab7654e4c183543b79391ba360a724b728da78
GQ\weigu <weiguoqiang2008@qq.com>
2022-01-19 22:54:22 +0800

    123

--- src/router/autoload.js
+++ src/router/autoload.js
@@ -1,6 +1,47 @@
-const layouts=import.meta.globEager('../layouts/*.vue');
-console.log(layouts);
+/*
+ * @Author: gq
+ * @Date: 2022-01-17 22:44:56
+ * @LastEditors: gq
+ * @LastEditTime: 2022-01-19 22:31:04
+ * @Description: file content
+ */
+const layouts = import.meta.globEager('../layouts/*.vue');
+const views = import.meta.globEager('../layouts/**/*.vue');
 
-const layoutRoutes=[]
+function getRoutes() {
+    const layoutRoutes = []
+    Object.entries(layouts).forEach(([file, module]) => {
+        const route = getRouteByModule(file, module);
+        route.children = getChildrenRoutes(route);
+        layoutRoutes.push(route);
+    })
 
-export default layoutRoutes;
+    return layoutRoutes;
+}
+
+
+function getChildrenRoutes(layoutRoute) {
+    console.log(layoutRoute);
+    Object.entries(views).forEach(([file, module]) => {
+        console.log(file);
+        if (file.includes(`../views/${layoutRoute.name}`)) {
+
+        }
+    })
+}
+
+function getRouteByModule(file, module) {
+    const name = file.replace(/.+layouts\/|\.vue/gi, '')
+    const route = {
+        name,
+        path: `/${name}`,
+
+        component: module.default
+    }
+    return route;
+
+}
+
+
+
+export default getRoutes();
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,28 @@
-import {createRouter,createWebHistory} from 'vue-router';
+/*
+ * @Author: gq
+ * @Date: 2022-01-17 22:44:56
+ * @LastEditors: gq
+ * @LastEditTime: 2022-01-19 18:52:47
+ * @Description: file content
+ */
+import { createRouter, createWebHistory } from 'vue-router';
 import layoutRoutes from './autoload';
-const router =createRouter({
-    history:createWebHistory(),
-    routes:[...layoutRoutes,
-        {
-            path:"/",
-            component:()=>import('../layouts/admin.vue'),
-            children:[{
-               path:"admin",
-               component:()=>import('../views/home.vue') 
-            }]
-        }
+console.log(layoutRoutes)
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [...layoutRoutes,
+    {
+        path: "/",
+        component: () => import('../layouts/admin.vue'),
+        children: [{
+            path: "admin",
+            component: () => import('../views/home.vue')
+        }]
+    }
     ]
 })
 
-export function setupRouter(app){
+export function setupRouter(app) {
     app.use(router);
 }
 
