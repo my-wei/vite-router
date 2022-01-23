cfc3118438b0122458710bb51671f691e854978d
my-wei <weiguoqiang2008@qq.com>
2022-01-23 22:28:59 +0800

    123123

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
+/*
+ * @Author: gq
+ * @Date: 2022-01-23 19:57:41
+ * @LastEditors: gq
+ * @LastEditTime: 2022-01-23 22:26:52
+ * @Description: file content
+ */
 import { defineConfig, loadEnv } from 'vite'
 
 import vue from '@vitejs/plugin-vue'
 import alias from './vite/alias';
 import { parseEnv } from './vite/utils';
 // https://vitejs.dev/config/
-// console.log(import.meta, 123)
 // export default defineConfig({
 //   plugins: [vue()],
-//   resolve:{
+//   resolve: {
 //     alias,
 //   }
 // })
@@ -15,7 +21,8 @@ import { parseEnv } from './vite/utils';
 export default ({ command, mode }) => {
   const isBuild = command = "build";
   const root = process.cwd();
-  const env =parseEnv(loadEnv(mode, root)) ;
+  const env = parseEnv(loadEnv(mode, root));
+
 
 
   console.log((env));
